828e3a04c59608206ac1e3672ae2e2b30724bee0
dianaelenap <48366074+dianaelenap@users.noreply.github.com>
2020-06-07 18:59:13 +0300

    Add files via upload

--- lists.php
+++ lists.php
@@ -7,7 +7,7 @@ include('lists-back.php');
 <head>
     <meta charset="utf-8">
     <title>Subcategory</title>
-    <link rel="stylesheet" href="style/lists.css">
+    <link rel="stylesheet" href="lists.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href='http://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' type='text/css'>
 </head>
--- productPage.php
+++ productPage.php
@@ -7,7 +7,7 @@
  <head>
      <meta charset="utf-8">
      <title>Subcategory</title>
-     <link rel="stylesheet" href="style/productPage.css">
+     <link rel="stylesheet" href="productPage.css">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
      <link href='http://fonts.googleapis.com/css?family=Merienda+One' rel='stylesheet' type='text/css'>
      <script type="text/javascript" src="javascript/functions.js"></script>
